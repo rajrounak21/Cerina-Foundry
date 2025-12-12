7ef85f2b47a4e841f14c332db8c9f35db8d6eca3
ROUNAK RAJ <rajrounak366@gmail.com>
2025-12-12 14:05:37 +0530

    Add files via upload

---
+++ finalizer.py
@@ -0,0 +1,7 @@
+def finalizer_node(state):
+    """
+    Finalizer node.
+    Persistence is now handled in main.py / invoke result processing 
+    to robustly include thread_id.
+    """
+    return state
